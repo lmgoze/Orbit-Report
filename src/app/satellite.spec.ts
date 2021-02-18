d0850f869aafc63ae011f4da912c23512b8e571e
Carly Langlois <cj.langlois.cl@gmail.com>
2021-02-18 16:20:32 -0600

    complete og instructions through #4

---
+++ src/app/satellite.spec.ts
@@ -0,0 +1,12 @@
+import { Satellite } from './satellite';
+
+describe('Satellite', () => {
+  it('should create an instance', () => {
+    expect(new Satellite()).toBeTruthy();
+  });
+
+  it('should create an instance', () => {
+	expect(new Satellite()).toBeTruthy();
+ });
+ 
+});
