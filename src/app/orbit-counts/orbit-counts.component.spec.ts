4838ff0f73e18fe22bbf8af4052aa90f36dd5f32
Carly Langlois <cj.langlois.cl@gmail.com>
2021-03-22 17:47:26 -0500

    add complete counts tet, remove html soln

--- src/app/orbit-counts/orbit-counts.component.spec.ts
+++ src/app/orbit-counts/orbit-counts.component.spec.ts
@@ -31,13 +31,21 @@ describe('OrbitCountsComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('NEW TEST', () => {
+  it('conditionally counts how many satellites are displayed', () => {
 	component.satellites = [new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true)];
 	fixture.detectChanges();
-	let counts = element.queryAll(By.css('.counts'));
-	//expect(numDataRows).toBe(1);
-	console.log(counts);
+	let counts = element.query(By.css('.counts-total'));
+	expect(counts.nativeElement.innerHTML).toBe('1');
 
+	component.satellites = [new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true), new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true)];
+	fixture.detectChanges();
+	counts = element.query(By.css('.counts-total'));
+	expect(counts.nativeElement.innerHTML).toBe('2');
+
+	component.satellites = [];
+	fixture.detectChanges();
+	counts = element.query(By.css('.counts-total'));
+	expect(counts.nativeElement.innerHTML).toBe('0');
 	
 
 	});
