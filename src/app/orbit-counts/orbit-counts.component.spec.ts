2ca6bee30f9d9618c1850d211b71744f29514e8e
Carly Langlois <cj.langlois.cl@gmail.com>
2021-03-22 17:07:17 -0500

    add a test to counts component

--- src/app/orbit-counts/orbit-counts.component.spec.ts
+++ src/app/orbit-counts/orbit-counts.component.spec.ts
@@ -1,10 +1,14 @@
+import { DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Satellite } from '../satellite';
 
 import { OrbitCountsComponent } from './orbit-counts.component';
 
 describe('OrbitCountsComponent', () => {
   let component: OrbitCountsComponent;
   let fixture: ComponentFixture<OrbitCountsComponent>;
+  let element: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -15,11 +19,26 @@ describe('OrbitCountsComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(OrbitCountsComponent);
-    component = fixture.componentInstance;
+    component = fixture.debugElement.componentInstance;
+	 element = fixture.debugElement;
+
+	 component.satellites = [];
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('NEW TEST', () => {
+	component.satellites = [new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true)];
+	fixture.detectChanges();
+	let counts = element.queryAll(By.css('.counts'));
+	//expect(numDataRows).toBe(1);
+	console.log(counts);
+
+	
+
+	});
 });
